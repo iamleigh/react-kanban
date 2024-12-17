f62a03b479fa0e1564b337ea1f621474f4982369
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-17 01:18:46 -0500

    feat: Base props for the kanban

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+import { JSX } from "react"
+
+export interface Card {
+	id: string | number
+	title?: string
+	description?: string
+	content?: JSX.Element
+}
+
+export interface Column<TCard extends Card> {
+	id: string | number
+	title: string
+	cards: TCard[]
+}
+
+export interface KanbanBoard<TCard extends Card> {
+	columns: Column<TCard>[]
+}
