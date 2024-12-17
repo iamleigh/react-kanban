3ac6d460ccccf7f949b4028f73f5279bfcc4ac64
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-16 23:35:40 -0500

    feat(card): Create new component
    Set the bases of a kanban card with title and description, and other properties that allow users to interact with it, such:
    - `dragging`
    - `allowRemoveCard`
    - `onCardRemove`

---
+++ src/components/Card.tsx
@@ -0,0 +1,32 @@
+import React, { JSX } from "react"
+
+interface CardProps {
+	title?: string
+	description?: string
+	dragging: boolean
+	allowRemoveCard: boolean
+	onCardRemove?: () => void
+}
+
+export const Card: React.FC<CardProps> = ({
+	title,
+	description,
+	dragging,
+	allowRemoveCard,
+	onCardRemove
+}) => {
+	return (
+		<div className={`kanban-card ${dragging ? 'kanban-card--dragging' : ''}`}>
+			<span>
+				<div className="react-kanban-card__title">
+					<span>{title}</span>
+					{ allowRemoveCard && (
+						<span style={{ cursor: 'pointer' }} onClick={onCardRemove ? () => onCardRemove() : undefined}>X</span>
+					)}
+				</div>
+			</span>
+
+			<div className="kanban-card__description">{ description }</div>
+		</div>
+	);
+}
